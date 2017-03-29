09a67160c0129c32531dfd7b517d8af0d66f3703
huai <myps91163@gmail.com>
2017-03-29 16:08:23 +0800

    initial commit

---
+++ nccu-course-firebase-master/lab-hosting/public/103703016/index.js
@@ -0,0 +1,19 @@
+function changeFontsize(target){
+	if ( target == "small" ){
+		document.getElementById("section").style.fontSize = "10px";
+	}
+	if ( target == "medium" ){
+		document.getElementById("section").style.fontSize = "14px";
+	}
+	if ( target == "large" ){
+		document.getElementById("section").style.fontSize = "16px";
+	}
+}
+
+function goToTop(target){
+    window.scrollTo(500, 0);
+}
+
+
+
+
